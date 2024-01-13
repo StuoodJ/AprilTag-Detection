e9da87bdc6630dc5d4e7088aab2fc55a0ec548f0
Jesse Betea <Jessebetea@gmail.com>
2024-01-13 03:32:04 -0500

    Update and rename Baselibraries.cpp to LimelightNetworkLibs.java

---
+++ LimelightNetworkLibs.java
@@ -0,0 +1,11 @@
+//Most of the code is from the documentation 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+public class Main {
+  
+  public static void main(String[] args) {
+  
+  }
+}
