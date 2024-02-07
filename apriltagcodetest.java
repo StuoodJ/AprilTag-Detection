2a7b7779e85144e8f1ad143692bbf17bca2df383
Jesse Betea <Jessebetea@gmail.com>
2024-02-07 16:31:15 -0500

    add botpose apriltagcodetest.java

--- apriltagcodetest.java
+++ apriltagcodetest.java
@@ -10,17 +10,21 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 main{
   public void{strings[] args) {
+    double botpose = LimelightHelpers.getBotpose("")
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry botpose = table.getEntry("botpose");
     NetworkTableEntry tx = table.getEntry("tx");
     NetworkTableEntry ty = table.getEntry("ty");
     NetworkTableEntry ta = table.getEntry("ta");
 
     //read values periodically
+    double pose = botpose.getDouble(0.0);
     double x = tx.getDouble(0.0);
     double y = ty.getDouble(0.0);
     double area = ta.getDouble(0.0);
 
     //post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightBotPose", pose);
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
     SmartDashboard.putNumber("LimelightArea", area);
